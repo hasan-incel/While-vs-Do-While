7711fade24f9f51453e54ad8f202d414c2c53437
Hasan İncel <h_incel@hotmail.com>
2024-07-24 11:44:55 +0300

    Add project files.

---
+++ While vs Do-While/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Aşağıdaki örnek hem while hem do while ile yapılmıştır.While döngüsü, koşulu döngünün başında kontrol eder. 
+Do-while döngüsü, koşulu döngünün sonunda kontrol eder. Bu nedenle, döngü en az bir kez çalışır, ardından koşul kontrol edilir. 
+ */
+
+Console.WriteLine("Lütfen input giriniz: ");
+int limit = Convert.ToInt32(Console.ReadLine());
+int sayac = 0;
+
+Console.WriteLine();
+Console.WriteLine("While");
+
+while (sayac < limit) 
+{
+    Console.WriteLine("Ben bir Patika'lıyım -->" + sayac);
+    sayac++;
+}
+
+Console.WriteLine();
+Console.WriteLine("Do-While");
+
+sayac = 0;
+do
+{
+    Console.WriteLine("Ben bir Patika'lıyım -->" + sayac);
+    sayac++;
+} while (sayac < limit);
